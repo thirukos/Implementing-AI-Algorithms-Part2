ae8a1393a4c69c225b3011f0083424f5c651303e
thirukos <thirukos@tcd.ie>
2023-04-09 20:49:08 +0100

    commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Implementing-AI-Algorithms-Part2
 Implementing-AI-Algorithms-To-Play-Games
+>> Both Tic-Tac-Toe and Connect4 has following mode
+    1) Human vs Human (TicTacToe.py, connectFour_AI.py)
+    2) Human vs Bot (TicTacToe.py, connectFour_AI.py)
+    3) Bot vs Minimax (TicTacToe.py, connectFour_AI.py)
+    4) Bot vs Q-Learning (TicTacToe.py, connectFour_AI.py)
+    5) Minimax vs Q-Learning (TicTacToe.py, connectFour_AI.py)
--- connect4/run.py
+++ connect4/run.py
@@ -1,33 +1,33 @@
-import pygame
-from connectFour import ConnectFour # connect 4 game class
-from connetFourBot import ConnectFourBot
-from connect4Minimax import ConnectFourMinimax
-import minimax
+# import pygame
+# from connectFour import ConnectFour # connect 4 game class
+# from connetFourBot import ConnectFourBot
+# from connect4Minimax import ConnectFourMinimax
+# import minimax
 
-def player_vs_player():
-    game = ConnectFour()
-    game.draw_board()
-    game.run_game()
+# def player_vs_player():
+#     game = ConnectFour()
+#     game.draw_board()
+#     game.run_game()
 
-def player_vs_bot():
-    game = ConnectFourBot(1)
-    game.draw_board()
-    game.run_game()
+# def player_vs_bot():
+#     game = ConnectFourBot(1)
+#     game.draw_board()
+#     game.run_game()
 
-def player_vs_minimaxAlgo():
-    game = ConnectFourMinimax()
-    game.draw_board()
-    game.run_game()
+# def player_vs_minimaxAlgo():
+#     game = ConnectFourMinimax()
+#     game.draw_board()
+#     game.run_game()
 
-def bot_vs_minimaxAlgo():
-    game = ConnectFourMinimax()
-    game.draw_board()
-    game.run_bot_vs_algorithm(bot_piece=1)
+# def bot_vs_minimaxAlgo():
+#     game = ConnectFourMinimax()
+#     game.draw_board()
+#     game.run_bot_vs_algorithm(bot_piece=1)
 
-if __name__ == '__main__':
-    pygame.init()
-    player_vs_player()
+# if __name__ == '__main__':
+#     pygame.init()
+#     player_vs_player()
     # player_vs_bot()
     # player_vs_minimaxAlgo()
     # bot_vs_minimaxAlgo()
-    pygame.quit()
+    # pygame.quit()
